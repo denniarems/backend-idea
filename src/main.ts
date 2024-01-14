440882f39fca934ffb66cca1b38e3e0867051ef1
Dennis <denniarems@gmail.com>
2024-01-14 12:36:29 +0530

    Add Docker ignore file and main.ts file

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { swagger } from '@elysiajs/swagger'
+import { logger } from '@grotto/logysia'
+import { Elysia } from 'elysia'
+import { autoroutes } from 'elysia-autoroutes'
+import { compression } from 'elysia-compression'
+import { helmet } from 'elysia-helmet'
+import { httpError } from 'elysia-http-error'
+import { ip } from 'elysia-ip'
+import { env } from 'env'
+// import { rateLimit } from 'elysia-rate-limit'
+
+const app = new Elysia()
+	.use(helmet())
+	.use(logger({ logIP: true }))
+	// .use(rateLimit({}))
+	.use(httpError())
+	.use(compression())
+	.use(ip())
+	.use(
+		swagger({
+			autoDarkMode: true
+		})
+	)
+	.use(
+		autoroutes({
+			routesDir: './src/routes' // -> optional, defaults to './routes'
+		})
+	)
+	.listen(env.PORT)
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
+console.log(`🦊 Swagger is running at ${app.server?.hostname}:${app.server?.port}/swagger`)
+
+export type ElysiaApp = typeof app
